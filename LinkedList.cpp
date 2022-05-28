ea6969ec7b8a0c9a7a108e5b9ba4a8ad0fd217ae
Kirill-Kuznetsov-git <kirill.cuznetsov200@yandex.ru>
2022-05-28 12:02:45 +0300

    add LinkedList

---
+++ LinkedList.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+class Node {
+public:
+    Node* left;
+    Node* right;
+    int value;
+};
+
+int main() {
+    Node* root;
+    root->value = 1;
+    return 0;
+}
